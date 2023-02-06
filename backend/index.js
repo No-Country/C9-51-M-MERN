40d5e0c2adc3bd56edb80e43be8d25741253d86d
Miguel <miguel.viloria.247@gmail.com>
2023-02-06 20:18:20 -0300

    endpoint registrar user 1

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+//Config express
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'))
+
+app.get('/', (req, res) => {
+    res.send('Todo Ok');
+    console.log('Todo Bien');
+});
+
+module.exports = app;
