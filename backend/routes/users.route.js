9ee8a9a035de1e57d6e501c34b63beb0788eb918
JorgeHeag1989 <Jorgehernanha@gmail.com>
2023-02-17 12:29:10 -0500

    user routes

--- backend/routes/users.route.js
+++ backend/routes/users.route.js
@@ -14,7 +14,7 @@ const usersRouter = express.Router();
 
 usersRouter.get("/", getAllUsers);
 usersRouter.post("/", createUser);
-usersRouter.get("/users/:id", getUserById);
+usersRouter.get("/:id", getUserById);
 usersRouter.patch("/:id", updateUser);
 usersRouter.delete("/:id", deleteUser);
 usersRouter.post('/login', login);
