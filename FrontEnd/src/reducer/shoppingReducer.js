e99a40099a2fbfe2347683c4a1d552bca5e26c6d
AntonelaBonaglia <107816695+AntonelaBonaglia@users.noreply.github.com>
2023-02-26 11:13:11 -0300

    "se modificaron componentes"

---
+++ FrontEnd/src/reducer/shoppingReducer.js
@@ -0,0 +1,55 @@
+// import { TYPES } from "../actions/shoppingActions";
+
+
+// export const shoppingInitialState = {
+//     products: [],
+//     cart: [],
+//     openCardModal: false,
+// };
+
+
+// export const shoppingReducer = (state, action) => {
+//     switch (action.type) {
+//         case TYPES.READ_STATE: {
+//             return {
+//                 ...state,
+//                 products: action.payload[0],
+//                 cart: action.payload[1]
+//             };
+//         }
+
+//         case TYPES.ADD_TO_CART: {
+
+//             const newItem = state.products.find(
+//                 (product) => product.id === action.payload);
+
+//             const itemInCart = state.cart.find((item) => item.id === newItem.id);
+            
+//             return (!itemInCart) 
+//                 ?{
+//                     ...state,
+//                     cart: [...state.cart, {newItem}] 
+//                 }
+//                 :{
+//                     ...state,
+//                     cart: [...state.cart]
+//                 }
+//             }
+
+//         case TYPES.OPEN_CARD_MODAL: {
+//             return state.openCardModal === false
+//                 ? { openCardModal: state.openCardModal = true }
+//                 : { ...state };
+//         }
+//         case TYPES.CLOSE_CARD_MODAL: {
+//             return state.openCardModal === true
+//                 ? { openCardModal: state.openCardModal = false }
+//                 : { ...state };
+//         }
+
+//         default:
+//             return state;
+//     }
+// };
+
+
--- FrontEnd/src/routes/Rutas.js
+++ FrontEnd/src/routes/Rutas.js
@@ -3,24 +3,20 @@ import Navigation from "../components/Navigation"
 // import ProductsSearch from "../components/ProductsSearch"  /* Sacar de las rutas, colocar buscador en navbar y resultados aqui luego de navigation */
 import CardList from "../components/CardList"
 import Description from "../components/Description"
-import CrudApi from "../components/CrudApi"
 import CargaTarjeta from "../components/CargaTarjeta"
 import TarjetaCD from "../components/TarjetaCD"
 import Main from "../components/Main"
 import Login from "../components/Login"
 import Register from "../components/Register"
-// import Contacto from "../components/Contacto"
+import Footer from "../components/Footer"
 // import ShoppingCart from "../components/shopping/ShoppingCart"
 // import Error404 from "../components/Error404"
-// import Footer from "../components/Footer";
-// import Footer from "../components/Footer/Footer"
 
 const Rutas = () => {
 
     return (
         <Router>
             <Navigation />
-            <CrudApi />
             <Routes>
                 <Route path="/" element={<Main />}/>
                 <Route path="/products" element={<CardList/>}/>
@@ -32,7 +28,7 @@ const Rutas = () => {
                 {/* <Route path="/carro" element={<ShoppingCart/>}/> */}
                 {/* <Route path="*" element={<Error404/> */}
             </Routes>            
-            {/* <Footer /> */}
+            <Footer />
         </Router>
     )
 }
